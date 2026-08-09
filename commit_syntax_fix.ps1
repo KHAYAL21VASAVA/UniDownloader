7cf7967c8ee803b342f25f6903f70d9831e4fa68
Khayal Vasava <khayal@example.com>
2026-08-09 23:47:31 +0530

    Update UniDownloader project files

--- commit_syntax_fix.ps1
+++
@@ -1,5 +0,0 @@
-$git = "C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin\git.exe"
-$env:PATH = "C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\usr\bin;" + $env:PATH
-& $git add .
-& $git commit -m "Fix syntax error: restore try block in stream handler"
-& $git log --oneline -n 2
