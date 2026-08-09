00ae3222712483a0aa3d964b9f9834d76c09cbda
Khayal Vasava <khayal@example.com>
2026-08-09 23:15:33 +0530

    Update UniDownloader project files

--- commit_mobile_fix.ps1
+++
@@ -1,5 +0,0 @@
-$git = "C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin\git.exe"
-$env:PATH = "C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\usr\bin;" + $env:PATH
-& $git add .
-& $git commit -m "Add smart link validation (reject generic homepages) and advanced mobile responsive design"
-& $git log --oneline -n 3
