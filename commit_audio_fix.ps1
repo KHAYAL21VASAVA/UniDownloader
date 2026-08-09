2a191705d0c39d574d82f9baf8edf1848b6b8039
Khayal Vasava <khayal@example.com>
2026-08-09 23:43:16 +0530

    Update UniDownloader project files

--- commit_audio_fix.ps1
+++
@@ -1,5 +0,0 @@
-$git = "C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin\git.exe"
-$env:PATH = "C:\Program Files\Git\cmd;C:\Program Files\Git\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin;C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\usr\bin;" + $env:PATH
-& $git add .
-& $git commit -m "Fix video stream selector: guarantee audio track is included in all MP4 downloads"
-& $git log --oneline -n 2
