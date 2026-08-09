1b4117d8b142dbbdd3d05141e2ecd15eab28513e
Khayal Vasava <khayal@example.com>
2026-08-09 21:58:47 +0530

    Clean working tree

--- git_init.ps1
+++
@@ -1,8 +0,0 @@
-$git = "C:\Users\KHAYAL VASAVA\AppData\Roaming\MobaXterm\slash\mx86_64b\bin\git.exe"
-& $git init
-& $git config user.name "Khayal Vasava"
-& $git config user.email "khayal@example.com"
-& $git add .
-& $git commit -m "Initial commit: UniDownloader Universal Media Downloader & Android PWA"
-& $git branch -M main
-& $git status
